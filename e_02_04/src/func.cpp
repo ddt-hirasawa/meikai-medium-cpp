cb41dd65055bdc894c09aa3f9112fad12b7805dc
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-15 20:02:52 +0900

    文字化けを修正しました

--- e_02_04/src/func.cpp
+++ e_02_04/src/func.cpp
@@ -1,10 +1,10 @@
 /*
- ���K2-4 �����Ȃ������̑� pos bit �` �� pos + n -1 �܂ł� n ���̃r�b�g��
- ���ꂼ�� 1 0 ���]�������l��Ԃ��֐��̍쐬
+ 演習2-4 符号なし整数の第 pos bit ～ 第 pos + n -1 までの n このビットを
+ それぞれ 1 0 反転させた値を返す関数の作成
 
- �쐬�� 2017�N5��15��
+ 作成日 2017年5月15日
 
- �쐬�� ���V�h��
+ 作成者 平澤敬介
  */
 
 #include<iostream>
@@ -12,59 +12,59 @@
 
 using namespace std;
 
-//�֐� �w�肵��bit����n��bit��1�ɏ��������܂�
-//������ �ϊ����鐮�� �w�肷��bit�̈ʒu �ϊ������
-//�ԋp�l �ω�����������
+//関数 指定したbitからn個のbitを1に書き換えます
+//仮引数 変換する整数 指定するbitの位置 変換する個数
+//返却値 変化させた整数
 
 unsigned set(unsigned tmp, int pos, int num) {
 
-	//�w�肵��bit�̈ʒu���� n�� ���ׂĂP�ɕϊ����܂�
+	//指定したbitの位置から n個分 すべて１に変換します
 	for (int i = pos; i < pos + num; i++) {
 
-		//���A���Ă���bit�� 1 �ł͂Ȃ��̂Ȃ�Ή��Z���s���܂�
+		//今、見ているbitが 1 ではないのならば演算を行います
 		if (!(tmp >> i & 1U)) {
 
-			tmp += pow(2, i);	//0 �� 1 �������܂� 2�̗ݏ悪 ����bit�� 1 �ɂȂ�
+			tmp += pow(2, i);	//0 に 1 を代入します 2の累乗が そのbitの 1 になる
 		}
 	}
 	return tmp;
 }
 
-//�֐� �w�肵��bit����n��bit��0�ɏ��������܂�
-//������ �ϊ����鐮�� �w�肷��bit�̈ʒu �ϊ������
-//�ԋp�l �ω�����������
+//関数 指定したbitからn個のbitを0に書き換えます
+//仮引数 変換する整数 指定するbitの位置 変換する個数
+//返却値 変化させた整数
 
 unsigned reset(unsigned tmp, int pos, int num) {
 
-	//�w�肵��bit�̈ʒu���� n�� ���ׂ�0�ɕϊ����܂�
+	//指定したbitの位置から n個分 すべて0に変換します
 	for (int i = pos; i < pos + num; i++) {
 
-		//���A���Ă���bit�� 0 �ł͂Ȃ��̂Ȃ�Ή��Z���s���܂�
+		//今、見ているbitが 0 ではないのならば演算を行います
 		if (tmp >> i & 1U) {
 
-			tmp -= pow(2, i);	//1 ���� 1 �������܂� 2�̗ݏ悪 ����bit�� 1 �ɂȂ�
+			tmp -= pow(2, i);	//1 から 1 を引きます 2の累乗が そのbitの 1 になる
 		}
 	}
 	return tmp;
 }
 
-//�֐� �w�肵��bit����n��bit�� 0 -> 1 �� 1 -> 0 �ɕϊ����܂�
-//������ �ϊ����鐮�� �w�肷��bit�̈ʒu �ϊ������
-//�ԋp�l �ω�����������
+//関数 指定したbitからn個のbitを 0 -> 1 に 1 -> 0 に変換します
+//仮引数 変換する整数 指定するbitの位置 変換する個数
+//返却値 変化させた整数
 
 unsigned inverse(unsigned tmp, int pos, int num) {
 
 	for (int i = pos; i < pos + num; i++) {
 
-			//�ύX���镔���� 0 �̎��̉��Z����
+			//変更する部分が 0 の時の演算部分
 			if (!(tmp >> i & 1U)) {
 
-				tmp += pow(2, i);		//�w�肵�� bit �̕����� 1 �ɏ��������܂�
+				tmp += pow(2, i);		//指定した bit の部分を 1 に書き換えます
 
-			//�ύX���镔���� 1 �̎��̉��Z����
+			//変更する部分が 1 の時の演算部分
 			} else if(tmp >> i & 1U){
 
-				tmp -= pow(2, i);		//�w�肵�� bit �̕����� 0 �ɏ��������܂�
+				tmp -= pow(2, i);		//指定した bit の部分を 0 に書き換えます
 			}
 	}
 	return tmp;
