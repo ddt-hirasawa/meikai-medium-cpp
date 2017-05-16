4daf622f3293c1e35d1d9452be99ee879e6b521d
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-16 20:37:34 +0900

    途中です 10%

---
+++ e_03_04/src/e_03_04.cpp
@@ -0,0 +1,26 @@
+/*
+演習3-4 台形公式によて積分した値を返す関数を作成せよ
+
+作成日 2017年5月16日
+
+作成者 平澤敬介
+*/
+
+#include<iostream>
+
+using namespace std;
+
+double drapezoid(double,double,int,double fp(double));
+
+int main()
+{
+
+
+
+
+	return 0;
+}
+
+//関数 台形公式により積分した値を返却します
+//仮引数
+//
