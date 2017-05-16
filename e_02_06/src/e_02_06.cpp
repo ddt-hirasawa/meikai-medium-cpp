6135552ccee0aca5488f4eeb412a633e5892606f
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-16 14:42:19 +0900

    途中です 80%

--- e_02_06/src/e_02_06.cpp
+++ e_02_06/src/e_02_06.cpp
@@ -1,48 +1,49 @@
 /*
-演習2-6 ここで学習した3つの型変換 同一ビットの型変換 長いビットへの型変換 短いビットへの型変換
-を確認できるプログラムの作成 変更前と変更後を表示せよ
+ 演習2-6 ここで学習した3つの型変換 同一ビットの型変換 長いビットへの型変換 短いビットへの型変換
+ を確認できるプログラムの作成 変更前と変更後を表示せよ
 
-作成日 2017年5月16日
+ 作成日 2017年5月16日
 
-作成者 平澤敬介
-*/
+ 作成者 平澤敬介
+ */
 
 #include<iostream>
+#include<math.h>
 
 using namespace std;
 
 //関数宣言
-const char* bits_of(unsigned tmp);
-int count_bits(unsigned tmp);
+template<class Type>
+const char* bits_of(Type tmp);
+template<class Type>
+int count_bits(Type tmp);
+
 //func.cppにまとめます
 unsigned change_unsigned(char* tmp_int);
-double change_double(char* tmp_int);
+short change_short(char* tmp_int);
+long long change_long_long(char* tmp_int);
 
 char tmp_short[16];			//静的記憶域間の文字列の配列を定義します ナル文字を含め17文字入ります 	  16bit
 char tmp_unsigned[32];		//静的記憶域間の文字列の配列を定義します ナル文字を含め33文字入ります 	  32bit
 char tmp_int[32];			//静的記憶域間の文字列の配列を定義します ナル文字を含め33文字入ります 	  32bit
-char tmp_double[64];		//静的記憶域間の文字列の配列を定義します ナル文字を含め65文字入ります     64bit
+char tmp_long_long[64];		//静的記憶域間の文字列の配列を定義します ナル文字を含め65文字入ります 	  64bit
 
-int main()
-{
+int main() {
+
 	int tmp = 43690;	//32bitの大きさの変数を定義し、0 1 の文字列にします 4 byte = 32bit
-	short tmp1;			//基準をint の変数としてそれぞれ bitの異なる変数に変換します
-	unsigned tmp2;		//short -> 16bit unsigned -> 32bit(符号無し) double -> 64bit(浮動小数点)
-	double tmp3;		//この変数にビットを変化させた値を格納します
-
 
 	//宣言 int型 32 bit から unsigned型32bit に変換します
 	cout << "同一bitへの変換\n";
 
 	//32bit なので 上位16bit は空になります 43690 が 2の16乗未満です
-	cout << "     int : " << bits_of(tmp) << " -> " << tmp << "\n";
+	cout << "      int : " << bits_of(tmp) << " -> " << tmp << "\n";
 
 	//32bit なので 上位16bit は空になります 43690 が 2の16乗未満です
-	cout << "unsigned : ";
+	cout << " unsigned : ";
 
 	//ビット数が変わらないので配列に格納されている上位ビットから下位ビットまで
 	//コピーし表示を行います
-	for(int i=0; i <= 31; i++) {
+	for (int i = 0; i <= 31; i++) {
 
 		tmp_unsigned[i] = tmp_int[i];	//ビットが変わらないので変化しません
 
@@ -54,25 +55,74 @@ int main()
 	cout << "\n";
 
 	//宣言 int型 32 bit から doubled型64bit に変換します
-	cout << "長いbitへの変換\n";
+	cout << "短いbitへの変換\n";
 
 	//32bit なので 上位16bit は空になります 43690 が 2の16乗未満です
-	cout << "     int : " << bits_of(tmp) << " -> " << tmp << "\n";
+	cout << "      int : " << bits_of(tmp) << " -> " << tmp << "\n";
 
 	//32bit から 64 bit の変換を行います double -> 64bit
-	cout << "  double : ";
+	cout << "    short :                 ";
 
 	//ビット数が変わらないので配列に格納されている上位ビットから下位ビットまで
 	//コピーし表示を行います
-	for(int i=0; i <= 63; i++) {
+	for (int i = 0; i < 16; i++) {
 
-		tmp_double[i] = tmp_int[i-31];	//ビットが変わらないので変化しません
+		tmp_short[i] = tmp_int[i + 16];	//ビットが変わらないので変化しません
 
-		cout << tmp_double[i];				//int と同じ表記になります
+		cout << tmp_short[i];				//int と同じ表記になります
 	}
 	//関数の呼び出し unsigned型に変化させた値を表示します
-	cout << " -> " << change_double(tmp_int);
+	cout << " -> " << change_short(tmp_int);
 	//区切ります
+	cout << "\n";
+
+	//宣言 int型 32 bit から long long型 64bit に変換します
+	cout << "長いbitへの変換\n";
+
+	//32bit なので 上位16bit は空になります 43690 が 2の16乗未満です
+	cout << "      int : " << bits_of(tmp) << " -> " << tmp << "\n";
+
+	//long long 型 64bit
+	cout << "long long : ";
+
+	//ビット数が変わらないので配列に格納されている上位ビットから下位ビットまで
+	//コピーし表示を行います
+	//長いビットへの変換なので 31 bit 目を確認し,1ならば 32 bit以降を 1で埋め尽くします
+	if (tmp >> 31 & 1U) {
+		//上位ビットをすべて変換します
+		for (int i = 0; i < 32; i++) {
+
+			tmp += pow(2,i + 32);		//ビットを1に変換するため 2の累乗を加算します
+
+			tmp_long_long[i] = '1';		//文字列を1で埋め尽くします
+
+			cout << tmp_long_long[i];	//64bit に変換し表記します
+		}
+	//長いビットへの変換なので 31 bit 目を確認し,0ならば 32 bit以降を 0で埋め尽くします
+	} else {
+		//上位ビットをすべて変換します
+		for (int i = 0; i < 32; i++) {
+
+			tmp_long_long[i] = '0';		//文字列を0で埋め尽くします
+
+			cout << tmp_long_long[i];	//64bit に変換し表記します
+		}
+	}
+
+	//下位ビットを変換します
+	for (int i = 32; i < 64; i++) {
+
+		tmp_long_long[i] = tmp_int[i - 32];	//ビットが変わらないので変化しません
+
+		cout << tmp_long_long[i];			//64bit に変換し表記します
+	}
+
+	//関数の呼び出し unsigned型に変化させた値を表示します
+
+	cout << " -> " << change_long_long(tmp_int);
+
+	//区切ります
+
 	cout << "\n";
 
 	return 0;
@@ -82,19 +132,46 @@ int main()
 //仮引数 bit 化する整数
 //返却値 そのbit化した整数の文字列の先頭のポインタ
 
-const char* bits_of(unsigned tmp) {
+template<class Type>
+const char* bits_of(Type tmp) {
 
-	int max_bit = count_bits(~0U) - 1;				//整数の全ビット数を求めます 0から始まるので1つ引きます
+	const char* ptr;	//返却する静的記憶域間の配列のポインタ
 
-	//上位ビットから下位ビットまで走査し文字列の配列を 1 0 で埋め尽くします
-	for(int i=max_bit; i>=0; i--) {
+	int max_bit = count_bits(~0U) - 1;			//整数の全ビット数を求めます 0から始まるので1つ引きます
 
-		*(tmp_int + max_bit-i) = ((tmp >> i) & 1U ? '1' : '0');	//整数 tmp のビットをシフトしその都度 1 0 を配列に入れます
+//32bit の時
+	if (sizeof(tmp) == 4) {
+
+		//上位ビットから下位ビットまで走査し文字列の配列を 1 0 で埋め尽くします
+		for (int i = max_bit; i >= 0; i--) {
+
+			*(tmp_int + max_bit - i) = ((tmp >> i) & 1U ? '1' : '0');//整数 tmp のビットをシフトしその都度 1 0 を配列に入れます
+		}
+
+		ptr = &*(tmp_int + 0);			//静的記憶域間の配列の先頭にポインタを設定します
+		//16bit の時
+	} else if (sizeof(tmp) == 2) {
+
+		//上位ビットから下位ビットまで走査し文字列の配列を 1 0 で埋め尽くします
+		for (int i = max_bit; i >= 0; i--) {
+
+			*(tmp_short + max_bit - i) = ((tmp >> i) & 1U ? '1' : '0');	//整数 tmp のビットをシフトしその都度 1 0 を配列に入れます
+		}
+
+		ptr = &*(tmp_short + 0);		//静的記憶域間の配列の先頭にポインタを設定します
+		//64bit の時
+	} else if (sizeof(tmp) == 8) {
+
+		//上位ビットから下位ビットまで走査し文字列の配列を 1 0 で埋め尽くします
+		for (int i = max_bit; i >= 0; i--) {
+
+			*(tmp_long_long + max_bit - i) = ((tmp >> i) & 1U ? '1' : '0');	//整数 tmp のビットをシフトしその都度 1 0 を配列に入れます
+		}
+
+		ptr = &*(tmp_long_long + 0);		//静的記憶域間の配列の先頭にポインタを設定します
+
 	}
-
-	const char* ptr = &*(tmp_int + 0) ;				//静的記憶域間の配列の先頭にポインタを設定します
-
-	//設定したポインタを返却し cout でビットを表示します
+//設定したポインタを返却し cout でビットを表示します
 	return ptr;
 }
 
@@ -102,13 +179,14 @@ const char* bits_of(unsigned tmp) {
 //仮引数 bit 数を検出したい整数
 //返却値 せの整数に '1' がある個数
 
-int count_bits(unsigned tmp) {
+template<class Type>
+int count_bits(Type tmp) {
 
 	int bits = 0;		//カウンタ bit は 0 か 1 0で初期化子カウントします
 
 	const int one = 1;	//1つシフトします 右に左に
 
-	//右にビットを移動していき、0になるまで繰り返します
+//右にビットを移動していき、0になるまで繰り返します
 	while (tmp) {
 
 		//ビットで 1 の部分があったとき
@@ -121,7 +199,7 @@ int count_bits(unsigned tmp) {
 		//tmp のbit を右に1つずらします
 		tmp >>= one;
 	}
-	//カウントしたbit数を返却します
+//カウントしたbit数を返却します
 	return bits;
 }
 
--- e_02_06/src/func.cpp
+++ e_02_06/src/func.cpp
@@ -23,7 +23,7 @@ unsigned change_unsigned(char* tmp_int) {
 	//int型の bit が格納されている文字列を使います
 	//31 -> 上位ビット 0 -> 下位ビット です
 	//上位から下位ビットまで繰り返します
-	for(int i=0; i <= 31; i++) {
+	for(int i=0; i < 32; i++) {
 
 		unsigned select = 0;	//bitの判別を行います
 
@@ -39,14 +39,56 @@ unsigned change_unsigned(char* tmp_int) {
 	return answer;
 }
 
-//関数 int型のbitからdoubled型の変数を作ります
+//関数 int型のbitからshort型の変数を作ります
 //仮引数 int型のbitが格納されている文字列の配列
-//返却値 int型をdouble型に変化させた値
+//返却値 int型をshort型に変化させた値
 
-double change_double(char* tmp_int) {
+short change_short(char* tmp_int) {
 
-	double answer = 0;
+	short answer = 0;
 
+	//int型の bit が格納されている文字列を使います
+	//31 -> 上位ビット 0 -> 下位ビット です
+	//上位から下位ビットまで繰り返します
+	for(int i=0; i < 16; i++) {
 
+		short select = 0;	//bitの判別を行います
+
+		//仮に、今見ているbitが 1 のとき
+		//1を代入します
+		if(tmp_int[i+16] == '1') {
+
+			select = 1;			//演算を行うので1の時2の累乗が加算されます
+		}
+
+		answer += pow(2,i) * select;	//bit から整数に戻します
+	}
+	return answer;
+}
+
+//関数 int型のbitからlong longt型の変数を作ります
+//仮引数 int型のbitが格納されている文字列の配列
+//返却値 int型をlong long型に変化させた値
+
+long long change_long_long(char* tmp_int) {
+
+	long long answer = 0;		//返却する long long型の変数を定義します 加算するので 0 で初期化
+
+	//int型の bit が格納されている文字列を使います
+	//64 -> 上位ビット 0 -> 下位ビット です
+	//上位から下位ビットまで繰り返します
+	for(int i=0; i < 64; i++) {
+
+		long long select = 0;	//bitの判別を行います
+
+		//仮に、今見ているbitが 1 のとき
+		//1を代入します
+		if(tmp_int[i] == '1') {
+
+			select = 1;			//演算を行うので1の時2の累乗が加算されます
+		}
+
+		answer += pow(2,i) * select;	//bit から整数に戻します
+	}
 	return answer;
 }
