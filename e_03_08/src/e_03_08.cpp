3c93fa12d39a1e22dba02ea003adca7f9c3bc739
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-17 20:03:04 +0900

    訂正 関数部分で＊か抜けていたので動作しませんでした

--- e_03_08/src/e_03_08.cpp
+++ e_03_08/src/e_03_08.cpp
@@ -24,11 +24,11 @@ int main() {
 	//乱数の種を生成
 	srand(time(NULL));
 
-	const int max = 10;		//配列の要素数を 100 に設定
+	const int max = 50;		//配列の要素数を 100 に設定
 
 	int key;				//探す数字 0
 
-	/*int search_array[max];		//関数を使って探す配列を作成
+	int search_array[max];
 
 	//要素数分乱数を発生させるためループ開始
 	for (int i = 0; i < max; i++) {
@@ -39,9 +39,6 @@ int main() {
 		cout << "array[" << setw(2) << i << "] = " << setw(2) << search_array[i]
 				<< "\n";
 	}
-	const_cast<const int>(search_array);*/
-
-	const int search_array[max] = {0,1,2,3,4,5,6,7,8,9};
 
 	//コンソールに表示される値を見て決めます
 	cout << "探す値 : ";
@@ -57,28 +54,34 @@ int main() {
 			//比較関数も返却値を強制キャストで変換しint型にします
 			reinterpret_cast<int (*)(const void*, const void*)>(int_result)));
 
-	cout << *(search_ptr - *search_array);
+	//ナル文字以外を指しているとき
+	if (search_ptr != NULL) {
 
+		//配列の何番目かを表示します
+		cout << "配列の" << search_ptr - search_array << "番目にあります\n";
+	//ありませんでした
+	} else {
+
+		//ナル文字の動作確認
+		cout << "ありません\n";
+	}
 }
 
-//関数 比較関数 tmp1 tmp2 で大きい 小さい 同じを判別して返却します
+//関数 比較関数 tmp1 tmp2 で同じかを判別して返却します
 //仮引数 整数2つ
-//返却値 1 -> tmp1 が大きい 0 -> 同じ -1 -> tmp2が大きい
+//返却値  1 -> 同じ
 
 int int_result(const int* tmp1, const int* tmp2) {
 
 	int answer = 0;		//返却値 同じ場合は 0
 
-	//tmp1 の方が大きい場合
-	if (tmp2 < tmp1) {
+	//tmp1 tmp2が等しい場合
+	if (*tmp2 == *tmp1) {
 
-		answer = 1;		//1を代入し,tmp1 が大きい
-		//tmp2 の方が大きい場合
-	} else {
-
-		answer = -1;	//-1を代入し,tmp2 が大きい
+		answer = 1;		//1を代入し,等しいことにする
 	}
-	//いずれにしても 1 0 -1 のどれかが返却される
+
+	//いずれにしても 1 0　どれかが返却される
 	return answer;
 }
 
@@ -92,22 +95,17 @@ void* seqsearch(const void* key, const void* base, size_t nmenb, size_t size,
 	//探すオブジェクトを強制キャストでポインタの文字列として変換した先頭とする
 	const char* tmp = reinterpret_cast<const char*>(base);
 
-	void* result;		//返却値 空のポインタの中身をNULLとする
-
 	//範囲が指定されている部分すべてを操作し一致する要素を探します
 	for (size_t i = 0; i < nmenb; i++) {
 
 		//関数により、keyと一致する部分がオブジェクトの中にある場合
-		if (!compar(key, reinterpret_cast<const void*>(&tmp[i * size]))) {
+		if (compar(key, reinterpret_cast<const void*>(&tmp[i * size]))) {
 
 			//返却値を定置属性を外した配列の要素の1つとします
-			result = const_cast<void*>(reinterpret_cast<const void*>(&tmp[i * size]));
-
-			//発見されたので終了します
-			break;
+			return const_cast<void*>(reinterpret_cast<const void*>(&tmp[i * size]));
 		}
 	}
 
 	//発見された場合は その部分へのポインタ ない場合はNULLを返却する
-	return result;
+	return NULL;
 }
