89eeb3321a66ba9598bd3c80812a372138e335f4
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-19 11:50:35 +0900

    訂正 先頭が返却されるように変更しました

--- e_03_10/src/e_03_10.cpp
+++ e_03_10/src/e_03_10.cpp
@@ -1,10 +1,10 @@
 /*
-演習3-10 binsearchx 関数を作成せよ
+ 演習3-10 binsearchx 関数を作成せよ
 
-作成日 2017年5月17日
+ 作成日 2017年5月17日
 
-作成者 平澤敬介
-*/
+ 作成者 平澤敬介
+ */
 
 #include<iostream>
 #include<cstdlib>
@@ -31,13 +31,17 @@ int main() {
 	int search_array[max];
 
 	//要素数分乱数を発生させるためループ開始
-	for (int i = 0; i < max; i++) {
+	for (int i = 0; i < max; i += 2) {
 
 		search_array[i] = i;//rand() % 50;		//0～49の間で乱数を発生 させた場合、乱数のためオーバーフロー
+
+		search_array[i + 1] = i;
 
 		//発生させた乱数を表示	ではなく	0　～　49まで降順
 		cout << "array[" << setw(2) << i << "] = " << setw(2) << search_array[i]
 				<< "\n";
+		cout << "array[" << setw(2) << i + 1 << "] = " << setw(2)
+				<< search_array[i + 1] << "\n";
 	}
 
 	//コンソールに表示される値を見て決めます
@@ -80,9 +84,8 @@ int int_result(const int* tmp1, const int* tmp2) {
 
 		answer = 1;		//1を代入し,tmp1が大きいことにする
 
-	//tmp2 が大きい場合
-	} else if(*tmp2 > *tmp1) {
-
+		//tmp2 が大きい場合
+	} else if (*tmp2 > *tmp1) {
 
 		answer = -1;	//-1を代入し,tmp2が大きいことにする
 	}
@@ -102,55 +105,57 @@ void* binsearch(const void* key, const void* base, size_t nmenb, size_t size,
 	//要素が1以上ないと探索できません
 	if (0 < nmenb) {
 
-		const char* tmp = reinterpret_cast<const char*>(base);	//探す文字列の先頭のポインタを保管します
+		const char* tmp = reinterpret_cast<const char*>(base);//探す文字列の先頭のポインタを保管します
 
-		size_t pl = 0;					//オブジェクトの先頭部分
+		size_t pl = 0;									//オブジェクトの先頭部分
 
-		size_t pr = nmenb - 1;			//オブジェクトの後端部分
+		size_t pr = nmenb - 1;							//オブジェクトの後端部分
 
-		size_t pc = (pl + pr) / 2;		//オブジェクトの中央付近
+		size_t pc = (pl + pr) / 2 + (pl + pr) % 2;		//オブジェクトの中央付近
 
 		//先端と後端が異なる限り続きます
-		for(;pl != pr;) {
+		for (; pl != pr;) {
 
-			pc = (pl + pr) / 2;		//オブジェクトの中央付近
+			pc = (pl + pr) / 2 + (pl + pr) % 2;		//オブジェクトの中央付近
 
-			int comp = compar(key,reinterpret_cast<const void*>(&tmp[pc * size]));	//ループごとに関数を呼び出し、0で発見したことに
-																					//なります
+			int comp = compar(key,
+					reinterpret_cast<const void*>(&tmp[pc * size]));//ループごとに関数を呼び出し、0で発見したことに
+																	//なります
 
 			//関数により発見したことがわかりました
-			if(comp == 0) {
+			if (comp == 0) {
 
-				//先頭の要素を探すため
-				for(; pl < pc; pc--) {
+				//1つ前の要素を確認します 仮に同じ要素ならやり直し テキストのfor文を書き換えます
+				if (compar(key,reinterpret_cast<const void*>(&tmp[(pc - 1) * size]))== 0) {
 
-					//関数で 0 が返却されて来ればループが終了します
-					if(!compar(key,reinterpret_cast<const void*>(&tmp[pc * size]))) {
+					//繰り返し同じ要素がないか探します
+					do{
+						pc--;		//1つ前の要素を探します
 
-						//先頭の要素を探すループから脱却
-						break;
-					}
+					//一つ前の要素と もう1つ前の要素が同じ場合くｒ
+					}while((&tmp[pc * size] == &tmp[(pc - 1) * size]));
 
 				}
 
 				//先頭の要素のポインタが代入されます
-				ptr = const_cast<void*>(reinterpret_cast<const void*>(&tmp[pc * size]));
+				ptr = const_cast<void*>(reinterpret_cast<const void*>(&tmp[pc
+						* size]));
 
 				//探索のループから脱却
 				break;
 
-			//先頭と後端が等しくなった時
-			} else if(pl == pr){
+				//先頭と後端が等しくなった時
+			} else if (pl == pr) {
 
 				//探索のループから脱却　失敗
 				break;
 
-			//先頭要素が大きかった場合
-			} else if(0 < comp){
+				//先頭要素が大きかった場合
+			} else if (0 < comp) {
 
 				pl = pc + 1;		//中央値を1つ後ろに進めます
 
-			//後端要素が大きかった場合
+				//後端要素が大きかった場合
 			} else {
 
 				pr = pc - 1;		//中央値を1つ前に進めます
