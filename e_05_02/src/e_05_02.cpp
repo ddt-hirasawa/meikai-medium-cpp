7c13147ae16bbd720569369f648a38c69f0371e9
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-22 18:05:10 +0900

    途中です90%

---
+++ e_05_02/src/e_05_02.cpp
@@ -0,0 +1,50 @@
+/*
+演習5-2 List5-18 の関数の引数をポインタから参照に変更すること
+
+作成日 2017年5月22日
+
+作成者 平澤敬介
+*/
+
+#include<iostream>
+#include"class.h"
+#include"vip.h"
+#include"senior.h"
+
+using namespace std;
+
+//関数宣言
+void senior_only(Member& tmp);
+
+int main()
+{
+	Member			name1("平澤 敬介",10,66);					//基底クラス
+	VipMember		name2("成田 直之",11,60,"会費免除");		//派生クラス
+	SeniorMember	name3("浅田 一志",12,49,1);					//派生クラス
+
+	//基底クラスのデータメンバの表示
+	senior_only(name1);
+
+	//派生クラスのデータメンバの表示
+	senior_only(name2);
+
+	//派生クラスのデータメンバの表示
+	senior_only(name3);
+
+	return 0;
+}
+
+//関数 派生先の中でもSeniorクラスのみ表示ができる関数
+//仮引数 Memberクラスのオブジェクトを指すポインタ
+//返却値 無し
+
+void senior_only(Member& tmp) {
+
+
+	Member& reference = dynamic_cast<SeniorMember*>(tmp);
+
+	if(*reference != NULL) {
+
+		reference.print();
+	}
+}
---
+++ e_05_02/src/vip.h
@@ -0,0 +1,68 @@
+/*
+演習5-2 List5-18 の関数の引数をポインタから参照に変更すること
+
+作成日 2017年5月22日
+
+作成者 平澤敬介
+*/
+
+#ifndef VIP_H_
+#define VIP_H_
+
+#include<iostream>
+#include<string>
+#include"class.h"
+
+//Vip会員クラス
+class VipMember : public Member{
+
+	//Vip特典	文字表記のみ
+	std::string privilege;
+
+public:
+	//コンストラクタ
+	VipMember(const std::string& name, int number_, double weight_, const std::string& prv ) :
+
+		//基底クラスをコンストラクタ初期化子により初期化
+		Member(name, number_, weight_) {
+
+		//メンバ関数により 特典を設定
+		set_prv(prv);
+	}
+
+	//仮想関数にした際、仮想関数のデストラクタを要求される？
+	virtual ~VipMember() {
+
+	}
+
+	//特典取得 VipMemberクラスのデータメンバのゲッタ
+	std::string get_prv() const {
+
+		return privilege;
+	}
+
+	//特典設定 VipMemberクラスのデータメンバのセッタ
+	void set_prv(const std::string& prv) {
+
+		//mainからの引数で入力がない場合未登録
+		privilege = (prv != "") ? prv : "未登録";
+	}
+
+	//関数 データメンバの表示 会員情報の開示
+	virtual void print() const {
+
+		//データメンバを見やすく表示 Member に特典を加えている
+		std::cout << "No." << number << " : " << full_name << "("
+				<< weight << "kg)" << "特典 = " << privilege << "\n";
+	}
+
+	//演習内容 自己紹介を行う仮想関数
+	virtual void introduce() const {
+
+		//派生クラスなので名前、番号、体重のデータに特典が追加されている
+		std::cout << "初めまして" << full_name << "です\n"
+				<< "VipMember なので  " << privilege << "があります\n";
+	}
+};
+
+#endif /* VIP_H_ */
