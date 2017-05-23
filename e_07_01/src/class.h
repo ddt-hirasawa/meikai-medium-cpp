d9f7b109efe8a58ab62e7a260007f60ee21361ff
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-23 15:22:43 +0900

    演習7-1 終了しました

---
+++ e_07_01/src/class.h
@@ -0,0 +1,96 @@
+/*
+演習7-1 クラス群を作成しクラスZ型のオブジェクトを初期化する際の基底クラス部分のオブジェクトの
+初期化の順序を示せ
+
+作成日 2017年5月23日
+
+作成者 平澤敬介
+*/
+
+#ifndef CLASS_H_
+#define CLASS_H_
+
+#include<iostream>
+
+//基底クラス 1
+class TestA {
+
+	int test;	//変数の定義
+
+public:
+
+	//コンストラクタ
+	TestA() :
+
+		test(10){
+
+		std::cout << "基底クラス 1 生成 データメンバ : " << test << "\n";
+	}
+};
+
+//基底クラス
+class TestB {
+
+	int test;	//変数の定義
+
+public:
+
+	//コンストラクタ
+	TestB() :
+
+		test(11){
+
+		std::cout << "基底クラス 2 生成 データメンバ : " << test << "\n";
+	}
+};
+
+//派生クラス 1
+class TestX : TestA , TestB{
+public:
+	int test;	//変数の定義
+
+	//コンストラクタ
+	TestX() :
+
+		TestA(), TestB(){
+
+		test = 12;
+
+		std::cout << "派生クラス 1 生成 データメンバ : " << test << "\n";
+	}
+};
+
+//派生クラス 2
+class TestY : TestB , TestA{
+public:
+	int test;	//変数の定義
+
+	//コンストラクタ
+	TestY() :
+
+		TestB(), TestA(){
+
+		test = 13;
+		std::cout << "派生クラス 2 生成 データメンバ : " << test << "\n";
+	}
+};
+
+//最終派生クラス
+class TestZ : TestX , TestY{		//クラスの宣言時 、X -> Y に向かって基底クラスからオブジェクトが
+//class TestZ : TestY , TestX{		//生成され派生クラス X Y が初期化された段階でクラス Zが初期化される
+public:
+	int test;	//変数の定義
+
+
+
+	//コンストラクタ
+	TestZ() :
+
+		TestX(),TestY(){
+
+		test = 14;
+		std::cout << "完成クラス 1 生成 データメンバ : " << test << "\n";
+	}
+};
+
+#endif /* CLASS_H_ */
