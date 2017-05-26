e3d79d9263336a72950922d405c47eefa9e89040
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-26 12:03:52 +0900

    新たにファイルを作り直しました

---
+++ e_09_02/src/e_09_02.cpp
@@ -0,0 +1,31 @@
+/*
+演習9-2 1-2でのDateクラスをテンプレート化せよ
+
+作成日 2017年5月25日
+
+作成者 平澤敬介
+*/
+
+#include"class.h"
+#include"date.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	Twin<Date<int> >  now(1 , 1);		//Date型のTwin
+										//2値での表示
+	//[ 日付 , 日付] になる
+	cout << "Date型のTwin\n" << now;
+
+	//区切ります
+	cout << "\n\n";
+
+	Twin < Twin < Date <int> > >  now1(Twin<Date<int> >(1,1) ,Twin<Date<int> >(1,1));//Date型のTwinのTwin
+
+	//[ [ 日付 , 日付] , //[ 日付 , 日付] ]になるになる
+	cout << "Date型のTwinのTwin\n" << now1;
+
+	return 0;
+}
