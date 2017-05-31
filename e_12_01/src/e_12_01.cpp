b7fc1346f9a4f067fdf71f175d8cbef876d6d994
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-31 13:45:37 +0900

    演習12－1途中です

--- e_12_01/src/e_12_01.cpp
+++ e_12_01/src/e_12_01.cpp
@@ -1,9 +1,9 @@
 /*
-貍皮ｿ�12-1 List 12-2 縺ｮ邯ｲ謗帙￠驛ｨ蛻�繧呈嶌縺肴鋤縺亥虚菴懊ｒ遒ｺ隱阪○繧�
+演習12-1 List 12-2 を書き換え動作の確認をせよ
 
-菴懈�先律 2017蟷ｴ5譛�31譌･
+作成日 2017年ｴ5月31日
 
-菴懈�占�� 蟷ｳ貔､謨ｬ莉�
+作成者 平澤敬介
 */
 
 #include<iostream>
@@ -12,32 +12,32 @@
 
 using namespace std;
 
-//髢｢謨ｰ螳｣險�
+//関数宣言
 bool file_exist(const char* filename);
 
 int main()
 {
-	string file_name;		//繝輔ぃ繧､繝ｫ縺ｮ蜷榊燕繧呈枚蟄怜�励→縺励※蜈･蜉帙☆繧句､画焚
+	string file_name;		//ファイル名をstring型で入力されます
 
-	cout << "縺ゅｋ縺九←縺�縺九ｒ遒ｺ隱阪＠縺溘＞繝輔ぃ繧､繝ｫ縺ｮ蜷榊燕 : ";
+	cout << "存在を確認したいファイルの名前 : ";
 
-	cin >> file_name;		//繝輔ぃ繧､繝ｫ縺ｮ蜷榊燕繧呈僑蠑ｵ蟄舌ｒ莉倥￠縺ｦ蜈･蜉�
+	cin >> file_name;		//文字列としてファイルの名前を入力
 
-	//髢｢謨ｰ繧貞他縺ｳ蜃ｺ縺励≠繧九° 辟｡縺�縺�?
-	cout << (file_exist(file_name.c_str()) ? ( "ある\n") : ("無い\n"));
+	//関数の呼び出し ファイルがあるかないか？
+	cout << (file_exist(file_name.c_str()) ? ( "true\n") : ("false\n"));
 
 	return 0;
 }
 
-//髢｢謨ｰ filename縺ｮ蜷榊燕繧呈戟縺､繝輔ぃ繧､繝ｫ縺後≠繧九°縺ｩ縺�縺九�ｮ蛻､蛻･繧定｡後≧
-//莉ｮ蠑墓焚 char*蝙九〒繝輔ぃ繧､繝ｫ縺ｮ蜷榊燕
-//霑泌唆蛟､ true or false
+//関数 ファイルがあるかどうかの判別
+//仮引数 ファイル名
+//返却値 true or false
 
 bool file_exist(const char* filename) {
 
-	ifstream file(filename);		//蜈･蜉帙せ繝医Μ繝ｼ繝�縺ｨ縺励※繧ｪ繝ｼ繝励Φ
+	ifstream file(filename);		//入力ストリームとしてオープン
 
-	return file.is_open();			//繧ｪ繝ｼ繝励Φ縺ｧ縺阪◆縺九←縺�縺具ｼ�
+	return file.is_open();			//オープンできたか？
 
 	//return file;
 	//return !file.fail();
