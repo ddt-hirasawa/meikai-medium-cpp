800a1dea2725f86a7491bc9ea2d0b03b873f1146
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-31 14:20:44 +0900

    演習12-2 終了しました

---
+++ e_12_02/src/e_12_02.cpp
@@ -0,0 +1,42 @@
+/*
+演習12-2 List12-2 を書き換え ifstream -> ofstream に変更せよ
+これにより、ファイルの内容が消去されることを確認せよ
+
+作成日 2017年5月31日
+
+作成者 平澤敬介平澤敬介
+*/
+
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+//関数宣言
+bool file_exist(const char* filename);
+
+int main()
+{
+	string file_name;		//ファイル名をstring型で入力されます
+
+	cout << "中身を削除したいファイルの名前 : ";
+
+	cin >> file_name;		//文字列としてファイルの名前を入力
+
+	//関数の呼び出し ファイルがあるかないか？
+	cout << (file_exist(file_name.c_str()) ? ( "true\n") : ("false\n"));
+
+	return 0;
+}
+
+//関数 ファイルがあるかどうかの判別
+//仮引数 ファイル名
+//返却値 true or false
+
+bool file_exist(const char* filename) {
+
+	ofstream file(filename);		//入力ストリームとしてオープン
+
+	return file.is_open();			//オープンできたか？
+}
