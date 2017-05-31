399265d50bfde1ed9d35f54e5fd8d9cf9175b6e0
hirasawa <ddt.jissyusei3@gmail.com>
2017-05-31 17:54:46 +0900

    基底クラス Origin を新たに作成しメンバ関数を継承するようにしました

--- e_06_03/src/derivation.h
+++ e_06_03/src/derivation.h
@@ -14,42 +14,17 @@
 #include<ctime>
 #include"class.h"
 
-class Human: public Player {
+//基底クラス 原点
+class Origin : public Player {
+public:
 
 	int hand;			//じゃんけんの手
 
-public:
-
-	//デフォルトコンストラクタ 最初はグー
-	Human() :
-			hand(0) {
-
-		std::cout << "最初はグー\n";
+	//コンストラクタ
+	Origin(int hand_) :
+		hand(hand_)
+	{
 	}
-
-	//コンストラクタ//
-	Human(int hand_) :
-
-			hand(hand_) {
-
-		//クラスオブジェクトが生成されたとき
-		//自分の手を表示します
-		switch (hand) {
-
-		case 0:
-			std::cout << "プレイヤー : グー\n";
-			break;
-
-		case 1:
-			std::cout << "プレイヤー : チョキ\n";
-			break;
-
-		case 2:
-			std::cout << "プレイヤー : パー\n";
-			break;
-		}
-	}
-
 
 	//メンバ関数
 	//じゃんけんの手をmain内に返却し勝ち負けを判断する
@@ -120,21 +95,59 @@ public:
 
 };
 
-class Computer: public Player {
+//クラス 人間
+class Human: public Origin {
 
-	int hand;			//じゃんけんの手
+public:
+
+	//デフォルトコンストラクタ 最初はグー
+	Human() :
+			Origin(0) {
+
+		std::cout << "最初はグー\n";
+	}
+
+	//コンストラクタ//
+	Human(int hand_) :
+
+			Origin(hand_) {
+
+		//クラスオブジェクトが生成されたとき
+		//自分の手を表示します
+		switch (hand) {
+
+		case 0:
+			std::cout << "プレイヤー : グー\n";
+			break;
+
+		case 1:
+			std::cout << "プレイヤー : チョキ\n";
+			break;
+
+		case 2:
+			std::cout << "プレイヤー : パー\n";
+			break;
+		}
+	}
+};
+
+//クラス コンピュータ
+class Computer: public Origin {
 
 public:
 
 	//デフォルトコンストラクタ 最初はグー
 	Computer() :
-			hand(0){
+			Origin(0){
 
 		std::cout << "じゃんけん\n";
 	}
 
 	//コンストラクタ//
-	Computer(int cpu) {
+	Computer(int cpu) :
+
+		Origin(0)
+	{
 
 		srand(time(NULL));
 
@@ -157,72 +170,6 @@ public:
 			break;
 
 		}
-	}
-
-	//純粋仮想関数
-	//じゃんけんの手をmain内に返却し勝ち負けを判断する
-	//返却値 データメンバじゃんけんの手
-	int output_hand()  const{
-
-		return hand;
-	}
-
-	//メンバ関数 プレイヤーの勝ち負けを判定
-	//仮引数 相手の手
-	//返却値 勝ち負け引き分けを 1 -1 0で返却
-
-	int judg(int tmp) const {
-
-		int answer;
-
-		//あいこの場合
-		if (tmp == hand) {
-
-			answer = 0;			//あいこ -> 0 を代入
-		//あいこ以外の時
-		} else {
-
-			switch (hand) {
-
-			//こちらがグーの時、相手がチョキなら
-			case 0:
-				if (tmp == 1) {
-
-					answer = 1;		//勝ち -> 1 を代入
-
-				} else {
-
-					answer = -1;	//負け -> -1 を代入
-				}
-				break;
-
-				//こちらがチョキの時、相手がパーなら
-			case 1:
-				if (tmp == 2) {
-
-					answer = 1;		//勝ち -> 1 を代入
-
-				} else {
-
-					answer = -1;	//負け -> -1 を代入
-				}
-				break;
-
-				//こちらがパーの時、相手がグーなら
-			case 2:
-				if (tmp == 0) {
-
-					answer = 1;		//勝ち -> 1 を代入
-
-				} else {
-
-					answer = -1;	//負け -> -1 を代入
-				}
-				break;
-
-			}
-		}
-		return answer;
 	}
 };
 
